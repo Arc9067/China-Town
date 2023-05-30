f7f65e2cb8ddc8f0faea8646e73108fbe446d554
Mikail Rokib <atandarokib@gmail.com>
2023-05-30 20:37:54 +0100

    updated

---
+++ src/Components/About.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import logo from "../assets/logo.png";
+
+const About = () => {
+  return (
+    <section className="py-[50px] w-full text-white">
+      <div className="container grid md:grid-cols-2 items-center justify-between gap-14">
+        <div className="order-2 md:order-1">
+          <img src={logo} alt="" className="w-full" />
+        </div>
+
+        <div className="flex flex-col gap-4 mt-10">
+          <h1
+            className="text-4xl break-all text-white  capitalize w-max"
+            data-aos="zoom-out"
+            data-aos-easing="linear"
+            data-aos-duration="1500"
+          >
+            about china town
+          </h1>
+          <p
+            data-aos="fade-right"
+            data-aos-easing="linear"
+            className="leading-relaxed mt-5 tracking-wider"
+          >
+            An image of the Statue of Liberty, proudly holding a toolbox, while
+            a bunch of shoddy products made in China lay broken around her.
+            Caption: "MIA: Because we believe in quality, not just quantity!"
+          </p>
+          <p
+            data-aos="fade-left"
+            data-aos-easing="linear"
+            className="leading-relaxed mt-5 tracking-wider"
+          >
+            A GIF of Uncle Sam rolling up his sleeves, repairing a broken toy
+            labeled "Made in China" with a confident smirk. Caption: "MIA:
+            Fixing the world, one product at a time!" 3 A side-by-side
+            comparison of an American flag-wrapped product labeled "Made in
+            America" and a flimsy Chinese knockoff. Caption: "Choose MIA for
+            durability and American pride!"
+          </p>
+          <p
+            className="leading-relaxed mt-5 tracking-wider"
+            data-aos="fade-right"
+            data-aos-easing="linear"
+          >
+            Remember, this is a fictional meme token created for fun and
+            entertainment purposes. Always make informed decisions when it comes
+            to investments and consider researching any product thoroughly
+            before purchasing. Enjoy the meme-filled ride with Made in America
+            (MIA) and let's celebrate quality and humor together! 🎉😄🇺🇸
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
---
+++ src/Components/Hero.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import Logo from "../assets/logo.png";
+
+const Hero = () => {
+  return (
+    <section className=" w-full pt-[150px] text-white">
+      <div className="container flex-col gap-6 font-Asiana flex justify-center items-center">
+        <h1
+          className="text-5xl tracking-widest leading-relaxed text-center  md:text-6xl font-Asiana capitalize font-bold"
+          data-aos="fade-"
+          data-aos-easing="linear"
+        >
+          china town
+        </h1>
+        <p
+          className="text-center tracking-wider leading-relaxed max-w-[500px] mx-auto text-2xl"
+          data-aos="fade-right"
+          data-aos-easing="linear"
+        >
+          Tired of cheap Chinese products flooding the market? Well, say hello
+          to "Made in America" (MIA) — the meme token that's here to give those
+          cheap products a run for their money, all while bringing a smile to
+          your face!
+        </p>
+        <a
+          href=""
+          className="bg-black capitalize text-2xl rounded-md px-6 py-3"
+          data-aos="fade-down"
+          data-aos-easing="linear"
+        >
+          buy china town
+        </a>
+        <div className="mt-10 flex flex-col gap-6 md:flex-row">
+          <a
+            href=""
+            className="bg-secondary capitalize text-3xl rounded-md px-6 py-3"
+            data-aos="fade-right"
+            data-aos-easing="ease"
+          >
+            telegram
+          </a>
+          <a
+            href=""
+            className="bg-black capitalize text-3xl rounded-md px-6 py-3"
+            data-aos="zoom-right"
+            data-aos-easing="linear"
+            data-aos-duration="2000"
+          >
+            twitter
+          </a>
+          <a
+            href=""
+            className="bg-secondary capitalize text-3xl rounded-md px-6 py-3"
+            data-aos="fade-left"
+            data-aos-easing="linear"
+          >
+            dextool
+          </a>
+          <a
+            href=""
+            className="bg-black capitalize text-3xl rounded-md px-6 py-3"
+            data-aos="fade-right"
+            data-aos-easing="linear"
+          >
+            medium
+          </a>
+        </div>
+
+        <img src={Logo} alt="logo" className="md:w-[15rem] object-cover" />
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
